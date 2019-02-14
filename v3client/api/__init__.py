e25dd2696672188ea8e1fb86c487a65ec40d43e7
Krypton Cougars <kryptoncougars@pasd.us>
2019-02-13 23:03:44 -0500

    TBA API, except it actually works.

---
+++ v3client/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from v3client.api.tba_api import TBAApi
+from v3client.api.district_api import DistrictApi
+from v3client.api.event_api import EventApi
+from v3client.api.list_api import ListApi
+from v3client.api.match_api import MatchApi
+from v3client.api.team_api import TeamApi
